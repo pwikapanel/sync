369573d2ede403444fb0d22db99e8310375ba4af
[andrewcswift] <[andrew@svija.love]>
2021-08-09 15:09:00 +0200

    working on news message when no connection

--- xcode/SvijaSync/Home/NewsView/NewsView.swift
+++ xcode/SvijaSync/Home/NewsView/NewsView.swift
@@ -19,8 +19,6 @@ class NewsView: NSView {
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    //textLabel.stringValue = ""
-    //messageLabel.stringValue = ""
     textLabel.isHidden = true
     messageLabel.isHidden = true
     
@@ -39,31 +37,11 @@ class NewsView: NSView {
   }
   
   private func update(_ data: NewsData) {
-    //textLabel.stringValue = data.text
-    //messageLabel.stringValue = data.message
     newsWebView.loadHTMLString(data.text, baseURL: nil)
   }
   
 }
 
-/*
- class PreferenceHelpView: NSView {
- @IBOutlet weak var helpWebView: WKWebView!
- 
- override func awakeFromNib() {
- super.awakeFromNib()
- loadContent()
- helpWebView.navigationDelegate = self
- }
- 
- private func loadContent() {
- helpWebView.loadHTMLString(html, baseURL: nil)
- helpWebView.setValue(false, forKey: "drawsBackground")
- toolTip = Text.Preference.Tooltip.visitLink
- }
- }
-
- */
 extension NewsView: WKNavigationDelegate {
   
   func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
@@ -79,26 +57,4 @@ extension NewsView: WKNavigationDelegate {
     }
   }
   
-}
-
-private extension NewsView {
-  
-  var html: String {
-    """
-        <html>
-        <head>
-        <style type=\"text/css\">
-            body{font-family: '-apple-system','HelveticaNeue'; font-size:13;}
-            a:link{text-decoration:none;color:#3273DC}a:visited{text-decoration:none;color:#3273DC}a:hover{text-decoration:none;color:#3273DC}a:active{text-decoration:none;color:#3273DC}
-            @media (prefers-color-scheme: dark) {
-            body{color: white;}
-            }
-        </style>
-        </head>
-        <body style="margin: 0;padding: 0">
-            \(Text.Preference.Help.html)
-        </body>
-        </html>
-        """
-  }
 }

bc43c62da2684be9a88777e3ec313c496984241b
Rajesh <rajesh8778@gmail.com>
2023-10-28 10:43:43 +1100

    Update logger path

--- Xcode/SvijaSync/Utility/Rsync/Models/SiteConnection.swift
+++ Xcode/SvijaSync/Utility/Rsync/Models/SiteConnection.swift
@@ -45,7 +45,7 @@ public extension SiteConnection {
 
     var localSyncPath: String { localPath + "/sync/" }
 	
-	var localSyncIdentityPath: String { localSyncPath + "/SVIJA/System/" }
+	var localSyncIdentityPath: String { localSyncPath + "SVIJA/System/" }
 		
 	var localLastModifiedFilePath: String { localSyncIdentityPath + "Last Modified.txt" }
 
